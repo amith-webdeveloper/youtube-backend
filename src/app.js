b6ff6cb2b2bc75dcd07c10c6649e81d8b610101d
amith-webdeveloper <amithwebdev1840@gmail.com>
2024-08-13 12:23:44 +0530

    written controllers and routes

--- src/app.js
+++ src/app.js
@@ -8,14 +8,19 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
-
-
 app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended:true , limit:"16kb"
-}))
-
+app.use(express.urlencoded({extended:true , limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+// routes
+// import routes
+
+import userRouter from './routes/user.routes.js'
+
+// routes declaration
+
+app.use("/users" , userRouter)
 
 
 
